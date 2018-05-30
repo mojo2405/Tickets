2d6818ef84dbee12d49696cc3db3e6d34029b9dc
Eitan Shaked <eitansh@mellanox.com>
2018-05-30 05:37:04 +0300

    Add menu to dislaimer page

--- app/src/main/java/il/co/myapp/tickets/MainActivity.java
+++ app/src/main/java/il/co/myapp/tickets/MainActivity.java
@@ -3,6 +3,8 @@ package il.co.myapp.tickets;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         cancelButton = findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,4 +34,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
 }
